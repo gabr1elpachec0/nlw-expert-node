e36c3fafbf920ebde6ae1480652b68eadcd33dec
gabr1elpachec0 <gabrielgpacheco12@gmail.com>
2024-02-13 11:29:47 -0300

    feat: add support for redis ranking system and list the pollOptions score

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -2,6 +2,7 @@ import { z } from "zod"
 import { randomUUID } from 'node:crypto'
 import { FastifyInstance } from "fastify"
 import { prisma } from "../../lib/prisma"
+import { redis } from "../../lib/redis"
 
 export async function voteOnPoll(app: FastifyInstance) {
   app.post('/poll/:pollId/vote', async (request, reply) => {
@@ -36,6 +37,8 @@ export async function voteOnPoll(app: FastifyInstance) {
             id: userPreviousVoteOnPoll.id
           }
         })
+
+        await redis.zincrby(pollId, -1, userPreviousVoteOnPoll.pollOptionId)
       } else if (userPreviousVoteOnPoll) {
         return reply.status(400).send({ message: 'You already voted on this poll.' })
       }
@@ -59,6 +62,8 @@ export async function voteOnPoll(app: FastifyInstance) {
         pollOptionId
       }
     })
+
+    await redis.zincrby(pollId, 1, pollOptionId)
 
     return reply.status(201).send({ sessionId })
   })
